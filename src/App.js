b1ed00dd766fb26ac449293f590031a3f11d29f4
jufrin01 <jufrinaha111@gmail.com>
2024-10-03 11:16:42 +0700

    make sure props , from hook >> UseState();

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './App.css';
 import Heading from "./components/Heading";
 
 
 function App() {
+
+    const [profiles, setProfiles] = useState([
+
+        {
+            id: 1,
+            name: "Huua",
+            age: 40
+        },
+        {
+            id: 2,
+            name: "Rajib",
+            age: 28
+        },
+        {
+            id: 3,
+            name: "Jufrin",
+            age: 27
+        },
+        {
+            id: 4,
+            name: "Kamal",
+            age: 35
+        },
+        {
+            id: 5,
+            name: "Davit",
+            age: 35
+        }
+
+    ])
   return (
     <div className="App">
-     <Heading></Heading>
-        <Heading></Heading>
-        <Heading></Heading>
-        <Heading></Heading>
+        {
+         profiles.map(profile => {
+            // const {id, name, age} = profile;
+           //  return <Heading key={id} name={name} age={age}> </Heading>
+             return (
+                 <Heading profile={profile}> </Heading>
+             )
+         })
+        }
 
     </div>
   );
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,10 +1,14 @@
 import React from'react';
 
-function Heading() {
+function Heading(props) {
+     // console.log(props);
+   // const {name , age} = props; // This will print the object in the Heading component
+    const { name , age } = props.profile; // This will print the object in the Heading component
+   // console.log(props); // This will print the text in the Heading component
     return (
         <div>
-            <h1>Heading 1</h1>
-            <p>paragraf 1</p>
+            <h1>Welcome Datang , {name}</h1>
+            <p>Age : {age}</p>
             <hr/>
         </div>
     )
