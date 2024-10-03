69c4df3336ccf3657a3e1f3e1855121e9a6011ca
jufrin01 <jufrinaha111@gmail.com>
2024-10-03 10:10:13 +0700

    tes Components

---
+++ src/components/Heading.js
@@ -0,0 +1,14 @@
+import React from'react';
+
+function Heading() {
+    return (
+        <div>
+            <h1>Heading 1</h1>
+            <p>paragraf 1</p>
+            <hr/>
+        </div>
+    )
+}
+
+export default Heading;
+
